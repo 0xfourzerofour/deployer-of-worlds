9464bfeaf3c18aff5104dc2ff810753e857e2ed3
Joshua Pauline <joshpauline@gmail.com>
2024-05-16 22:23:12 -0400

    feat(actions): start specing out how actions should work

--- crates/deployer/src/action.rs
+++ crates/deployer/src/action.rs
@@ -1,8 +1,50 @@
+use alloy::{
+    json_abi::JsonAbi,
+    primitives::{Address, Bytes, B256},
+};
+
 #[derive(Debug)]
 pub enum ActionType {
     Deployment,
     Write,
-    ConditionalWrite,
+    Read,
+}
+
+pub struct DeploymentData {
+    address: Address,
+    constructor_args: Vec<String>,
+    salt: B256,
+    abi: JsonAbi,
+    bytecode: Bytes,
+}
+
+pub struct WriteData {
+    address: Address,
+    args: Vec<String>,
+    value: B256,
+    condition: Option<WriteCondition>,
+}
+
+pub struct WriteCondition {
+    action_id: String,
+    cmp: CpmOption,
+}
+
+enum CpmOption {
+    Neq,
+    Eq,
+    Gt,
+    Gte,
+    Lt,
+    Lte,
+}
+
+pub struct ReadData {
+    address: Address,
+    constructor_args: Vec<String>,
+    salt: B256,
+    abi: JsonAbi,
+    bytecode: Bytes,
 }
 
 #[derive(Debug)]
