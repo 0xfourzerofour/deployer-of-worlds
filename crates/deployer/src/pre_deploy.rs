8f9652b156d8887844ffd67427ec82e1118e7833
Joshua Pauline <joshpauline@gmail.com>
2024-05-19 16:24:37 -0400

    feat(actions): remove post and predeploy files

--- crates/deployer/src/pre_deploy.rs
+++
@@ -1,8 +0,0 @@
-use alloy_core::{json_abi::JsonAbi, primitives::Address, primitives::Bytes};
-
-#[derive(Debug)]
-pub struct PreDeploy {
-    address: Address,
-    function_sig: String,
-    args: Vec<String>,
-}
